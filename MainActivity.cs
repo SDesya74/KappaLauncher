d848dc1e3c3a4b4a129b4df4be16fd467e79c362
Саня Череп <SDesya74@gmail.com>
2019-10-07 14:02:55 +0300

    Deleted layout folder

--- MainActivity.cs
+++ MainActivity.cs
@@ -13,8 +13,8 @@ namespace KappaLauncher
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
