f844970d2b161a61a6b98a1b022e8a7244c3ad99
Саня Череп <SDesya74@gmail.com>
2019-10-07 20:19:30 +0300

    Added

---
+++ LauncherApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using KappaLauncher.Application.Launcher;
+using KappaLauncher.Misc;
+
+namespace KappaLauncher {
+    [Application]
+    public class LauncherApplication : Android.App.Application {
+        public LauncherApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) {
+        }
+
+        public override void OnCreate() {
+            base.OnCreate();
+
+            Context context = ApplicationContext;
+
+            Screen.Init(context);
+            Launcher.Init(context);
+        }
+    }
+}
