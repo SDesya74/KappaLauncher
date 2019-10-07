e50c8eb28b3e38e02b0cd21823dc6d6e7163b90e
Саня Череп <SDesya74@gmail.com>
2019-10-07 20:17:33 +0300

    Added

---
+++ Application/Misc/Screen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.Util;
+using Android.Views;
+
+namespace KappaLauncher.Misc {
+    public static class Screen {
+        public static int Width { get; private set; }
+        public static int Height { get; private set; }
+        public static float Scale { get; private set; }
+        public static int ActionBarHeight { get; private set; }
+        public static int StatusBarHeight { get; private set; }
+        public static int UsefulHeight {
+            get {
+                return Height - ActionBarHeight;
+            }
+        }
+
+        public static int Dip2Px(float dip) {
+            return (int) Math.Floor(dip * Scale);
+        }
+        public static void Init(Context context) {
+            IWindowManager wm = (IWindowManager) context.GetSystemService(Context.WindowService);
+
+            Display display = wm.DefaultDisplay;
+            DisplayMetrics metrics = new DisplayMetrics();
+            display.GetMetrics(metrics);
+
+            Point point = new Point();
+            display.GetSize(point);
+            Width = point.X;
+            Height = point.Y;
+
+            Scale = context.Resources.DisplayMetrics.Density;
+
+            TypedArray ta = context.Theme.ObtainStyledAttributes(new int[] { Android.Resource.Attribute.ActionBarSize });
+            ActionBarHeight = (int) ta.GetDimension(0, 0);
+
+            int resourceId = context.Resources.GetIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) StatusBarHeight = context.Resources.GetDimensionPixelSize(resourceId);
+        }
+
+        public static int Dip(this int px) {
+            return Dip2Px(px);
+        }
+    }
+}
