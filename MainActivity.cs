706f618a92dbd57b77eaad5dda3c2c903d4bfbab
Саня Череп <SDesya74@gmail.com>
2019-10-08 01:53:50 +0300

    Some shanges

--- MainActivity.cs
+++ MainActivity.cs
@@ -3,27 +3,25 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+
 using KappaLauncher.Application.Launcher;
-using KappaLauncher.Misc;
+using KappaLauncher.Application.Misc;
 
-namespace KappaLauncher
-{
+namespace KappaLauncher {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
+    public class MainActivity : AppCompatActivity {
+        protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-
-            Screen.Init(this);
             Launcher.Show(this);
             Launcher.Load();
+
+            string readed = (string) DataSaver.Read("test");
+            if (readed == null) {
+                DataSaver.Save("test", "Konichiwa sekai!");
+            } else Toast.MakeText(this, readed, ToastLength.Short).Show();
         }
 
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
