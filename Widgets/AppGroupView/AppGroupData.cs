602027faa03ce15696568de89b07558f10363931
Саня Череп <SDesya74@gmail.com>
2019-10-10 23:53:24 +0300

    Some changes

--- Widgets/AppGroupView/AppGroupData.cs
+++ Widgets/AppGroupView/AppGroupData.cs
@@ -9,11 +9,17 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using KappaLauncher.Apps;
 
 namespace KappaLauncher.Widgets {
-	class AppGroupData : Launcher.WidgetData{
-		
 
+	[Serializable]
+	class AppGroupData : Launcher.WidgetData{
+		public List<AppDrawingData> Items { get; private set; }
+
+		public AppGroupData(List<AppManager.App> list) {
+			list.ForEach( e => Items.Add(new AppDrawingData(e)));
+		}
 		
 	}
 }
