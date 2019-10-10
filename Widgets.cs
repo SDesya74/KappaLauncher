e19512b1de845b66edfd546f2bac7a02e7241bd9
Саня Череп <SDesya74@gmail.com>
2019-10-10 19:44:03 +0300

    Added

---
+++ Widgets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KappaLauncher {
+	public static partial class Launcher {
+		public static List<WidgetData> Widgets;
+
+
+		[Serializable]
+		public abstract class WidgetData { };
+
+	}
+}
---
+++ Widgets/AppGroupView/AppGroupView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+
+namespace KappaLauncher.Widgets {
+	class AppGroupView : View {
+		public AppGroupView(Context context) : base(context) {
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+		public class Element {
+
+		}
+	}
+}
