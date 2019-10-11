394df8fed891e20aa30015708d488756f9d07e8d
Саня Череп <SDesya74@gmail.com>
2019-10-11 23:53:13 +0300

    Some changes

--- Widgets/AppGroupView/AppGroupData.cs
+++ Widgets/AppGroupView/AppGroupData.cs
@@ -10,15 +10,22 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using KappaLauncher.Apps;
+using KappaLauncher.Misc;
 
 namespace KappaLauncher.Widgets {
 
 	[Serializable]
 	class AppGroupData : Launcher.WidgetData{
 		public List<AppDrawingData> Items { get; private set; }
+		public int RowMargin { get; set; }
+		public int ColumnMargin { get; set; }
 
 		public AppGroupData(List<AppManager.App> list) {
-			list.ForEach( e => Items.Add(new AppDrawingData(e)));
+			Items = new List<AppDrawingData>();
+			list.ForEach(e => Items.Add(new AppDrawingData(e)));
+
+			RowMargin = 3.Dip();
+			ColumnMargin = 3.Dip();
 		}
 		
 	}
