80e29d23b7759419a40fe2a81abb100b87ad4607
Саня Череп <SDesya74@gmail.com>
2019-10-12 23:19:22 +0300

    Some changes

--- Misc/DataSaver.cs
+++ Misc/DataSaver.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using Android.Content
 
 namespace KappaLauncher.Misc {
     class DataSaver {
