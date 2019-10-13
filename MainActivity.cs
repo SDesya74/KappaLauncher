25acf3a5f364d0ffeba5d92a8873bcdbeb1ce206
Саня Череп <SDesya74@gmail.com>
2019-10-13 22:29:24 +0300

    Ech

--- MainActivity.cs
+++ MainActivity.cs
@@ -16,7 +16,6 @@ namespace KappaLauncher {
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
