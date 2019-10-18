d40128a41ee33b7c60482ccb30cd08a4cdc4a8d9
Саня Череп <SDesya74@gmail.com>
2019-10-18 23:43:21 +0300

    Some changes

--- MainActivity.cs
+++ MainActivity.cs
@@ -15,6 +15,7 @@ namespace KappaLauncher {
             Launcher.Show(this);
             Launcher.Load();
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
