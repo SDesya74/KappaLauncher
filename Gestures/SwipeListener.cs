e88f933f5124a853eda915bf132be63d5529e6ae
Саня Череп <SDesya74@gmail.com>
2019-10-18 23:38:54 +0300

    Added OnSwipe method

--- Gestures/SwipeListener.cs
+++ Gestures/SwipeListener.cs
@@ -6,7 +6,23 @@ using Android.Views;
 using Android.Widget;
 
 namespace KappaLauncher.Gestures {
-	class SwipeListener : BaseGestureListener{
-		
+	class SwipeListener : BaseGestureListener {
+		public override void OnClick(View view, MotionEvent e, long time) {}
+
+		public override void OnMove(View view, MotionEvent e) {}
+
+		public override void OnTouchEnd(View view, MotionEvent e) {}
+
+		public override void OnTouchStart(View view, MotionEvent e) {}
+
+
+		public delegate void OnSwipeListener(Direction direction);
+		public event OnSwipeListener OnSwipe;
+
+
+
+		public enum Direction {
+			LEFT, RIGHT
+		}
 	}
 }
