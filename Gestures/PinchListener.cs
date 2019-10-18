b145e35a4fd84062cde7ac61e8c028e9d59f9037
Саня Череп <SDesya74@gmail.com>
2019-10-18 23:48:52 +0300

    Added

---
+++ Gestures/PinchListener.cs
@@ -0,0 +1,23 @@
+﻿using Android.Views;
+
+namespace KappaLauncher.Gestures {
+	class PinchListener : BaseGestureListener {
+		public override void OnClick(View view, MotionEvent e, long time) { }
+
+		public override void OnMove(View view, MotionEvent e) { }
+
+		public override void OnTouchEnd(View view, MotionEvent e) { }
+
+		public override void OnTouchStart(View view, MotionEvent e) { }
+
+
+		public delegate void OnPinchListener(Direction direction);
+		public event OnPinchListener OnPinch;
+
+
+
+		public enum Direction {
+			In, Out
+		}
+	}
+}
