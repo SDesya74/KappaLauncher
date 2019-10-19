beac90539e7e20495dad317a28c23d01a24da7bd
Саня Череп <SDesya74@gmail.com>
2019-10-19 15:12:05 +0300

    Added calc distance method & added on touch

--- Gestures/BaseGestureListener.cs
+++ Gestures/BaseGestureListener.cs
@@ -1,20 +1,26 @@
 ﻿using Android.Graphics;
 using Android.Views;
+using System;
 
 namespace KappaLauncher.Gestures {
 	public abstract class BaseGestureListener{
 
 		public bool IsGesture;
 		public long TouchStartTime;
-		public PointF TouchStart;
-		public PointF TouchCurrent;
-		
 
-		public bool OnTouch(View view, MotionEvent e) {
-			TouchCurrent = new PointF(e.GetX(), e.GetY());
-			switch(e.Action) {
+		private float Distance(PointF first, PointF second) {
+			float dx = second.X - first.X;
+			float dy = second.Y - first.Y;
+
+			return MathF.Sqrt(dx * dx + dy * dy);
+		}
+
+
+		public bool OnTouchEvent(View view, MotionEvent e) {
+			OnTouch(view, e);
+
+			switch(e.Action) {	
 				case MotionEventActions.Down:
-					TouchStart = new PointF(e.GetX(), e.GetY());
 					TouchStartTime = Java.Lang.JavaSystem.CurrentTimeMillis();
 					IsGesture = false;
 
@@ -26,7 +32,7 @@ namespace KappaLauncher.Gestures {
 					long time = Java.Lang.JavaSystem.CurrentTimeMillis() - TouchStartTime;
 					TouchStartTime = -1;
 
-					OnTouchEnd(view, e);
+					OnTouchEnd(view, e, time);
 					if(!IsGesture) OnClick(view, e, time);
 					return true;
 
@@ -40,11 +46,11 @@ namespace KappaLauncher.Gestures {
 			return false;
 		}
 
-
+		public abstract void OnTouch(View view, MotionEvent e);
 
 		public abstract void OnTouchStart(View view, MotionEvent e);
 
-		public abstract void OnTouchEnd(View view, MotionEvent e);
+		public abstract void OnTouchEnd(View view, MotionEvent e, long time);
 
 		public abstract void OnMove(View view, MotionEvent e);
 
