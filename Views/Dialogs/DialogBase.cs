e88db661eaa4e55f2b121a20ca2435db16a8f1bb
Саня Череп <SDesya74@gmail.com>
2019-10-21 23:46:09 +0300

    Some changes

--- Views/Dialogs/DialogBase.cs
+++ Views/Dialogs/DialogBase.cs
@@ -11,7 +11,32 @@ using Android.Views;
 using Android.Widget;
 
 namespace KappaLauncher.Views.Dialogs {
-	class DialogBase {
-		
+	abstract class DialogBase {
+		private PopupWindow Window;
+		private Context Context;
+		public DialogBase(Context context) {
+			Window = new PopupWindow();
+			Context = context;
+		}
+
+		public abstract View OnCreate(Context context);
+
+
+		public bool IsShowing {
+			get {
+				return (bool) Window?.IsShowing;
+			}
+		}
+
+		public void Show() {
+			View view = OnCreate(Context);
+			Window = new PopupWindow(view, ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+			Window?.ShowAtLocation(Launcher.Parent, GravityFlags.Center, 0, 0);
+		}
+
+		public void Dismiss() {
+			Window?.Dismiss();
+			Window?.Dispose();
+		}
 	}
 }
