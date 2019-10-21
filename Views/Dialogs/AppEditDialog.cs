7398d6a99ead17713d46eff68c52767cd1a5c394
Саня Череп <SDesya74@gmail.com>
2019-10-21 23:46:14 +0300

    Some changes

--- Views/Dialogs/AppEditDialog.cs
+++ Views/Dialogs/AppEditDialog.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
+﻿using Android.Content;
 using Android.Views;
 using Android.Widget;
+using KappaLauncher.Apps;
+using KappaLauncher.Misc;
 
 namespace KappaLauncher.Views.Dialogs {
 	class AppEditDialog : DialogBase {
+
+		public AppEditDialog(Context context, AppDrawingData data) : base(context) {
+
+		}
+
+		public override View OnCreate(Context context) {
+			LinearLayout main = new LinearLayout(context);
+			main.Orientation = Orientation.Vertical;
+			main.SetPadding(5.Dip(), 5.Dip(), 5.Dip(), 5.Dip());
+			main.SetBackgroundColor(Android.Graphics.Color.Rgb(255, 255, 255));
+			return main;
+		}
 	}
 }
