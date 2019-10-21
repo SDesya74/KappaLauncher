05224d713fb58919cb34277b0cc8e15c42663fec
Саня Череп <SDesya74@gmail.com>
2019-10-21 23:05:57 +0300

    Renamed

--- Widgets/AppGroup/AppGroupWidget.Row.cs
+++ Widgets/AppGroup/AppGroupWidget.Row.cs
@@ -5,7 +5,7 @@ using KappaLauncher.Apps;
 using System;
 using System.Collections.Generic;
 
-namespace KappaLauncher.Widgets {
+namespace KappaLauncher.Widgets.AppGroup {
 	partial class AppGroupWidget {
 		public List<Row> Rows { get; private set; }
 
